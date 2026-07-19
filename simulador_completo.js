c4cdb2a5a5a5fec2d5cc9a077ba982156b787e69
01BARRETO1 <karbar_1990@hotmail.com>
2026-07-18 23:20:13 -0500

    Parte B, buscar y actualizar

--- simulador_completo.js
+++ simulador_completo.js
@@ -1,15 +1,15 @@
 
-  let clientes = [];
-  let creditos = [];
+let clientes = [];
+let creditos = [];
 
-  let tasaInteres = 15;
-  let clienteSeleccionado = null;
-  let cuotaCalculada = 0;
-  let montoCalculado = 0;
-  let plazoCalculado = 0;
-  let creditoAprobado = false;
+let tasaInteres = 15;
+let clienteSeleccionado = null;
+let cuotaCalculada = 0;
+let montoCalculado = 0;
+let plazoCalculado = 0;
+let creditoAprobado = false;
 
-  
+
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
 
 //💻 Manipulación de clases con JavaScript
@@ -27,25 +27,25 @@
 //
 //️Actividades
 //a) Crear función ocultarSecciones()
-function ocultarSecciones(){
+function ocultarSecciones() {
   document.getElementById("parametros").classList.remove("activa");
   document.getElementById("clientes").classList.remove("activa");
 }
 //b) Crear función mostrarSeccion(id)
-function mostrarSecciones(id){
+function mostrarSecciones(id) {
   ocultarSecciones();
   document.getElementById(id).classList.add("activa");
 
 }
 //PARTE 2: CONFIGURAR TASA
 
-function guardarTasa(){
+function guardarTasa() {
 
-  tasaInteres=recuperarInt("tasaInteres")
-  if(tasaInteres>=10 && tasaInteres<=20){
-    mostrarTexto("mensajeTasa","✅ Tasa configurada correctamente: "+tasaInteres+"%")
-  }else{
-    mostrarTexto("mensajeTasa","❌ La tasa debe estar entre 10% y 20%")
+  tasaInteres = recuperarInt("tasaInteres")
+  if (tasaInteres >= 10 && tasaInteres <= 20) {
+    mostrarTexto("mensajeTasa", "✅ Tasa configurada correctamente: " + tasaInteres + "%")
+  } else {
+    mostrarTexto("mensajeTasa", "❌ La tasa debe estar entre 10% y 20%")
   }
 
 }
@@ -54,54 +54,118 @@ function guardarTasa(){
 //🧱 PARTE A: CREAR Y LISTAR CLIENTES
 //let clientes = []; está creadad arriba como global
 //️Función guardarCliente()
-function guardarCliente(){
-  
+function guardarCliente() {
+  //ÚLTIMO PASO DEL DOCUMENTO SIMULADOR 2 
+  /* ✔ Si NO existe → crear
+    ✔ Si existe → actualizar (excepto cédula)*/
+
   //1. Obtener datos del formulario
-  let cedula = recuperaraTexto("txtCedula")
-  let nombre=recuperaraTexto("txtNombre");
-  let apellido=recuperaraTexto("txtApellido");
+  let cedula = recuperaraTexto("txtCedula");
+
+
+  let nombre = recuperaraTexto("txtNombre");
+  let apellido = recuperaraTexto("txtApellido");
   //2. Convertir valores numéricos
-  let ingresos= recuperarFloat("txtIngresos");
-  let egresos= recuperarFloat("txtEgresos");
-  
-  //3. Crear objeto cliente
-  let cliente={};
-  //4. Agregarlo al arreglo
-  cliente.cedula=cedula;
-  cliente.nombre=nombre;
-  cliente.apellido=apellido;
-  cliente.ingresos=ingresos;
-  cliente.egresos=egresos;
-  clientes.push(cliente);
-  //Cada vez que se guarda un cliente → llamar pintarClientes()
-  pintarClientes();
+  let ingresos = recuperarFloat("txtIngresos");
+  let egresos = recuperarFloat("txtEgresos");
+  //✔ Si NO existe → crear
+  //Variable del objeto si lo encuntra
+  let busquedaCliente = buscarCliente(cedula);
+  if (busquedaCliente == null) {
+    //3. Crear objeto cliente
+    let cliente = {};
+    //4. Agregarlo al arreglo
+    cliente.cedula = cedula;
+    cliente.nombre = nombre;
+    cliente.apellido = apellido;
+    cliente.ingresos = ingresos;
+    cliente.egresos = egresos;
+    clientes.push(cliente);
+    pintarClientes();
+  } else {
+    alert("cliente ya existe, Actualizando...");
+    //document.getElementById("txtCedula");
+    //document.getElementById("txtCedula").readOnly = true;
+    //seleccionarCliente(cedula);
+    clienteSeleccionado.nombre = nombre;
+    clienteSeleccionado.apellido = apellido;
+    clienteSeleccionado.ingresos = ingresos;
+    clienteSeleccionado.egresos = egresos;
+    pintarClientes()
+  }
 }
 
 //Función pintarClientes()
-function pintarClientes(){
-  
+function pintarClientes() {
+
   //Recorrer el arreglo
-  let idTablitaDinamica=document.getElementById("tablaClientes");
-  let tablitaDinamica="";
+  let idTablitaDinamica = document.getElementById("tablaClientes");
+  let tablitaDinamica = "";
   let persona;
-  for(let i=0; i<clientes.length; i++){
+  for (let i = 0; i < clientes.length; i++) {
     //Generar filas <tr> dinámicamente
-    persona=clientes[i];
+    persona = clientes[i];
     //3.-Insertarlas en: <tbody id="tablaClientes"></tbody>
-    tablitaDinamica+= "<tbody id='tablaClientes'>"+
-          "<tr>"
-    tablitaDinamica+="<td>"+persona.cedula+"</td>"+
-          "<td>"+persona.nombre+"</td>"+
-          "<td>"+persona.apellido+"</td>"+
-          "<td>"+persona.ingresos+"</td>"+
-          "<td>"+persona.egresos+"</td>"+
-          "<td>"+
-            "<button>Actualizar</button>"+
-          "</td>"+
-        "</tr>"+
-        "</tbody>"
+    tablitaDinamica += "<tr>"
+    tablitaDinamica += "<td>" + persona.cedula + "</td>" +
+      "<td>" + persona.nombre + "</td>" +
+      "<td>" + persona.apellido + "</td>" +
+      "<td>" + persona.ingresos + "</td>" +
+      "<td>" + persona.egresos + "</td>" +
+      "<td>" +
+      "<button onclick='seleccionarCliente(\"" + persona.cedula + "\");'>Actualizar</button>" +
+      "</td>" +
+      "</tr>" +
+      "</tbody>"
   }
-  idTablitaDinamica.innerHTML=tablitaDinamica;
+  idTablitaDinamica.innerHTML = tablitaDinamica;
 }
 
-//VALIDAR--Validaciones
+//VALIDAR--Validaciones --VALIDACIONES_CLIENTES.JS
+//
+//PARTE B: BUSCAR Y ACTUALIZAR
+//Función buscarCliente(cedula)
+function buscarCliente(cedula) {
+  let cedulaEncontrada;
+  let clienteExiste = null;
+  for (let i = 0; i < clientes.length; i++) {
+    cedulaEncontrada = clientes[i];
+    if (cedulaEncontrada.cedula == cedula) {
+      clienteExiste = cedulaEncontrada;
+      console.log("Ya existe");
+      return clienteExiste;
+    }
+
+  }
+  return clienteExiste;
+}
+//
+//Función seleccionarCliente(cedula)
+function seleccionarCliente(cedula) {
+  let cedulaSeleccionada = cedula
+  let buscarCliente;
+  for (i = 0; i < clientes.length; i++) {
+    buscarCliente = clientes[i]
+    if (cedulaSeleccionada == buscarCliente.cedula) {
+      clienteSeleccionado = buscarCliente
+      alert("Actualizar datos de: " + clienteSeleccionado.cedula);
+      //document.getElementById("txtCedula").readOnly = true;
+      mostrarTextoEnCaja("txtCedula", clienteSeleccionado.cedula);
+      mostrarTextoEnCaja("txtNombre", clienteSeleccionado.nombre);
+      mostrarTextoEnCaja("txtApellido", clienteSeleccionado.apellido);
+      mostrarTextoEnCaja("txtIngresos", clienteSeleccionado.ingresos);
+      mostrarTextoEnCaja("txtEgresos", clienteSeleccionado.egresos);
+    }
+  }
+  return clienteSeleccionado;
+
+
+}
+
+function limpiar() {
+  mostrarTextoEnCaja("txtCedula", "");
+  mostrarTextoEnCaja("txtNombre", "");
+  mostrarTextoEnCaja("txtApellido", "");
+  mostrarTextoEnCaja("txtIngresos", "");
+  mostrarTextoEnCaja("txtEgresos", ""); 
+}
